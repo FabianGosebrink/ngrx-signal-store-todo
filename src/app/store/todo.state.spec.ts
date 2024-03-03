831806b1cd0d4369e1a0e4d3ce0dd135b5942fbc
FabianGosebrink <fabian.gosebrink@outlook.com>
2024-03-03 11:08:49 +0100

    Update function to handle edge case

---
+++ src/app/store/todo.state.spec.ts
@@ -0,0 +1,32 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { provideMock } from '../../../testing/auto-mock';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
+import { TodoStore } from './todo.state';
+
+describe('TodoStore', () => {
+  let service: TodoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TodoStore, provideMock(TodoService)],
+    });
+
+    service = TestBed.inject(TodoService);
+  });
+
+  describe('with Methods', () => {
+    it('should update store when item is moved to done', waitForAsync(() => {
+      // arrange
+      const store = TestBed.inject(TodoStore);
+      const item = { id: '1', value: 'test', done: false } as Todo;
+      const spy = spyOn(store, 'update');
+
+      // act
+      store.moveToDone(item);
+
+      // assert
+      expect(spy).toHaveBeenCalledWith({ ...item, done: true });
+    }));
+  });
+});
